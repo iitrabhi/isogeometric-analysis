87e870b07fae8ed4a693f0f6f6c5b1236c195170
Abhinav Gupta <iitrabhi@gmail.com>
2023-12-19 22:05:43 -0600

    Update index.js

--- lessons/12_mobile_two/index.js
+++ lessons/12_mobile_two/index.js
@@ -1,3 +1,11 @@
+// To enable your circle created with Two.js to be 
+// draggable both with a mouse and with touch on 
+// mobile devices, you need to handle both mouse events 
+// and touch events. The basic events you'll be looking at 
+// are mousedown and touchstart for the initial contact, 
+// mousemove and touchmove for the dragging, and mouseup and 
+// touchend for releasing the drag.
+
 // Assuming you have a Two.js instance and a circle
 var two = new Two({ fullscreen: true }).appendTo(document.body);
 var circle = two.makeCircle(70, 70, 50);
