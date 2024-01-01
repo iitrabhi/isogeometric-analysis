e88ff1234dc93a0c04cac990c9a25ac4b31bc6af
Abhinav Gupta <iitrabhi@gmail.com>
2024-01-01 09:41:26 -0600

    update

--- lessons/12_mobile_two/index.js
+++ lessons/12_mobile_two/index.js
@@ -9,7 +9,7 @@
 // Assuming you have a Two.js instance and a circle
 var two = new Two({ fullscreen: true }).appendTo(document.body);
 var circle = two.makeCircle(70, 70, 50);
-circle.fill = '#FFFFFF';
+circle.fill = '#888888';
 circle.noStroke();
 
 
